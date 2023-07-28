a7cfdb4bcdb8d30ecb8ba20e2450f74d57e57708
AldoTorrez <adlotorrez91@gmail.com>
2023-07-28 18:17:49 -0400

    ultimos cambios de codigo

--- client/src/Redux/action.js
+++ client/src/Redux/action.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-export const videoGames =()=>{
-    return async (dispatch)=> {
-        try {
-          const response = await axios.get('http://localhost:3001/videogames');
-          const videogames = response.data;
-          
-          dispatch({ type: 'ALL_VIDEOGAMES', payload: videogames });
-        } catch (error) {
-          // Manejo de errores
-          dispatch({ type: 'FETCH_ERROR', payload: error.message });
-        }
-      };
+export const videoGames = ()=>{
+  return async(dispatch)=>{
+    try{
+      const response = await axios.get('http://localhost:3001/videogames');
+      const videogame = response.data;
+      dispatch({type: 'ALL_VIDEOGAMES', payload: videogame});
+    }
+    catch(error){
+      throw Error(error);
+    }
+  }
 }
 
 export const nameFilter = (inputValue)=>{
